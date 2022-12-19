b72081b344c94d13ef3a13f2914376c355d6e08d
Андрей Солодянкин <andrsolo21@gmail.com>
2022-12-20 00:56:58 +0300

    gitignore

--- sql_scripts.sql
+++ sql_scripts.sql
@@ -1,13 +1,5 @@
-
-
 psql -U ml_user -d ml_models
 
 select * from models;
 
 drop table MODELS;
-
-SELECT MODELNAME FROM MODELS WHERE MODELS.IS_DELETED = FALSE;
-
-SELECT "MODELNAME" FROM MODELS WHERE "ISDELETED" = FALSE AND "MODELTYPE" = 'Lasso';
-
-SELECT * FROM pg_catalog.pg_tables WHERE tablename = 'models';
